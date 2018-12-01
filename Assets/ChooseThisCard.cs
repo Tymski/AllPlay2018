beec6b265583399b7b093457dda69c1448925f1c
Tymski <tymon.oziemblewski@gmail.com>
2018-12-01 19:21:51 +0100

    Można wybierać karty

---
+++ Assets/ChooseThisCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseThisCard : MonoBehaviour {
+
+	public PlayerCardChoose playerCardChoose;
+
+	public void ChooseMe () {
+		playerCardChoose.chooseCard (GetComponent<CardUI> ().data);
+	}
+}
---
+++ Assets/PlayerCardChoose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCardChoose : MonoBehaviour {
+
+	public CardUI[] cards;
+	public int iterator = 0;
+
+	public void chooseCard (CardData cardData) {
+		if (iterator >= 3) return;
+		CardUI card = cards[iterator];
+		card.data = cardData;
+		card.changeData ();
+		card.gameObject.SetActive (true);
+		iterator++;
+	}
+
+}
