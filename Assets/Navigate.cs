d05fe50fda138f958bfe10cf0a0ff6df5075a5ae
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2018-12-01 13:42:58 +0100

    Added raw sounds files

---
+++ Assets/Navigate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Navigate : MonoBehaviour {
+	public void OnClickHandler() {
+		SceneManager.LoadScene("MainMenu");
+	}
+}
