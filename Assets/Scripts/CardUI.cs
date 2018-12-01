697c6a9e7e278586175cc0191cf3ebcc988135bf
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2018-12-01 05:01:52 +0100

    Cards view

---
+++ Assets/Scripts/CardUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUI : MonoBehaviour {
+
+	public Image cardIcon;
+	public TextMeshProUGUI cardName;
+	public TextMeshProUGUI cardDescription;
+
+	public CardData data;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
