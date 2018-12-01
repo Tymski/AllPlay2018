4fda3929bdefe100a0edd3515f8fed5b3a18d7ab
Tymski <tymon.oziemblewski@gmail.com>
2018-12-01 19:41:43 +0100

    random monster cards

--- Assets/ChooseThisCard.cs
+++ Assets/ChooseThisCard.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseThisCard : MonoBehaviour {
 
 	public PlayerCardChoose playerCardChoose;
+	public bool thisCardWasChosen;
 
 	public void ChooseMe () {
+		if(thisCardWasChosen)return;
 		playerCardChoose.chooseCard (GetComponent<CardUI> ().data);
+		GetComponent<Button>().interactable = false;
+		thisCardWasChosen = true;
 	}
 }
