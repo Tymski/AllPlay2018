72f83976064a1a769c368306343a56c7da6bf7ff
Tymski <tymon.oziemblewski@gmail.com>
2018-12-01 16:45:07 +0100

    PickARoom

---
+++ Assets/PickARoom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickARoom : MonoBehaviour {
+
+	void Start () {
+		foreach(Transform t in transform){
+			t.gameObject.SetActive(false);
+		}
+		int rng = Random.Range(0,transform.childCount);
+		transform.GetChild(rng).gameObject.SetActive(true);
+	}
+	
+
+}
