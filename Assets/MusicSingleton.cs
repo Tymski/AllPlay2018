971958f930f909ba780840163e2436100bb37ab5
Tymski <tymon.oziemblewski@gmail.com>
2018-12-02 08:45:07 +0100

    11099

---
+++ Assets/MusicSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSingleton : MonoBehaviour {
+
+	public static MusicSingleton Instance;
+	void Start () {
+		if (Instance == null) {
+			Instance = this;
+		}
+		if (Instance != this) {
+			Destroy (gameObject);
+		} else {
+			DontDestroyOnLoad (gameObject);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/OnChooseSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnChooseSound : MonoBehaviour {
+
+	public AudioClip audioSample;
+	AudioSource audioSource;
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void PlaySound () {
+		audioSource.PlayOneShot (audioSample);
+	}
+}
